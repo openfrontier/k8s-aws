8214540e3c79255b3e28dfa251aa957c7beaeb07
thinkernel <thinkernel@gmail.com>
2018-11-23 06:01:16 +0000

    merge eip resource into ec2 file

--- k8s-cluster/ec2.tf
+++ k8s-cluster/ec2.tf
@@ -74,3 +74,14 @@ resource "aws_instance" "demo01" {
     }
   }
 }
+
+resource "aws_eip" "demo01" {
+  count    = "${var.ec2_instance_count}"
+  instance = "${element(aws_instance.demo01.*.id, count.index)}"
+  vpc      = true
+
+  tags {
+    Name = "${var.cluster_name}-${count.index}"
+    "kubernetes.io/cluster/aws.devops.demo" = "owned"
+  }
+}

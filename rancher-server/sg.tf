c399bef9d9b4b7a9bff2b8af67f68f0e4458d087
李 征辉 <li.zhenghui@trans-cosmos.com.cn>
2018-12-14 09:43:10 +0800

    modify(module import) rollback

--- rancher-server/sg.tf
+++ rancher-server/sg.tf
@@ -1,39 +1,50 @@
-module "rancher_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
+resource "aws_security_group" "demo01" {
   name        = "rancher-k8s"
   description = "Allow ssh inbound traffic"
-  vpc_id      = "${module.vpc.default_vpc_id}"
+  vpc_id      = "${aws_vpc.demo01.id}"
 
-  ingress_with_cidr_blocks = [
-    {
-      rule        = "ssh-tcp"
-      cidr_blocks = "${var.sg_inbound_ips}"
-    },
-    {
-      rule        = "http-8080-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-    {
-      rule        = "http-80-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-  ingress_with_self = [
-    {
-      rule = "all-all"
-    },
-  ]
+  # terraform ssh provisioners
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["${var.sg_inbound_ip_tf}", "${var.sg_inbound_ip_ssh}"]
+  }
 
-  egress_with_cidr_blocks = [
-    {
-      rule = "all-all"
-    },
-  ]
+  # rancher port
+  ingress {
+    from_port   = "8080"
+    to_port     = "8080"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # http port
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # access from same vpc
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = "true"
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags {
     Name = "${var.cluster_name}"
     "kubernetes.io/cluster/k8s" = "owned"
   }
-
 }
--- rancher-server/variables.tf
+++ rancher-server/variables.tf
@@ -18,7 +18,8 @@ variable "instance_type" {
   default = "t2.micro"
 }
 variable "vpc_cidr_block" {}
-variable "sg_inbound_ips" {}
+variable "sg_inbound_ip_ssh" {}
+variable "sg_inbound_ip_tf" {}
 variable "os_type" {}
 variable "root_volume_type" {
   default = "gp2"
